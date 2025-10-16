03da931c571c4e3872c1a4f05821d0f6e88d1371
wngydls-lang <wngydls@sookmyung.ac.kr>
2025-10-16 15:16:49 +0900

    7주차 실습1

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int flag = 0;
+  int y;
+  while(flag != 0)
+  {
+    y = 3;
+    flag = 0;
+  }
+  y = 4;
+  
+  printf("flag: %d, y: %d\n", flag, y);
+  
+  system("PAUSE");	
+  return 0;
+}
