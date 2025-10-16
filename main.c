ed87af4b660f42c183b1033a896fa3caeb96e42f
wngydls-lang <wngydls@sookmyung.ac.kr>
2025-10-16 15:29:43 +0900

    실습2-1

--- main.c
+++ main.c
@@ -5,11 +5,11 @@ int main(int argc, char *argv[])
 {
   int i;
   
-  for(i=0;i<5;i++)
+  for (i=0;i<5;i++)
   {
-       int temp = 1;
-       printf("temp = %d\n", temp);
-       temp++;
+      static int temp = 1;
+      printf("temp = %d\n", temp);
+      temp++;
   }
   
   system("PAUSE");	
