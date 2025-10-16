001c721a3e7d171449b26d9c1fb3edc886a047b0
wngydls-lang <wngydls@sookmyung.ac.kr>
2025-10-16 15:36:23 +0900

    실습3

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 
 int main(int argc, char *argv[])
 {
-  int i;
+  int i = 10;
+  printf("before i = %d\n", i);
+  i = inc(i); //반환값을 이용  
+  printf("after i = %d\n", i);
   
-  for (i=0;i<5;i++)
-  {
-      static int temp = 1;
-      printf("temp = %d\n", temp);
-      temp++;
-  }
-  
-  system("PAUSE");	
+  system("PAUSE");
   return 0;
 }
+int inc(int counter)
+{
+    counter++;
+    return counter;
+}
+
