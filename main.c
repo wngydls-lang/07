7b6d270f3f55fc971e42e7b5db3770122b94dd06
wngydls-lang <wngydls@sookmyung.ac.kr>
2025-10-16 15:44:38 +0900

    실습4

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-  int i = 10;
-  printf("before i = %d\n", i);
-  i = inc(i); //반환값을 이용  
-  printf("after i = %d\n", i);
-  
-  system("PAUSE");
-  return 0;
-}
-int inc(int counter)
-{
-    counter++;
-    return counter;
-}
+int counter;
 
+void set_counter()
+{
+     counter = 20;
+}
+int main(void)
+{
+    printf("counter = %d\n", counter); //default = 0
+    set_counter();
+    printf("counter = %d\n", counter);
+    
+    system("PAUSE");
+    return 0;
+} 
